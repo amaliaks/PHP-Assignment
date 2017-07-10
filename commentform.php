9ced848b6fae1a61c0540d8aa533a11ddd8d695d
amaliaks <aak120@georgetown.edu>
2017-07-10 19:59:29 -0400

    Update commentform.php

--- commentform.php
+++ commentform.php
@@ -3,7 +3,7 @@
   <!-- Code is based on W3Schools PHP Forms-->
   I love hearing from you! Leave a comment with your name and email and I'll be sure to write you back! 
 
-  <?php echo $_POST["name"];?>!<br>
+  Name: <?php echo $_POST["name"];?>!<br>
   Email Address: <?php echo $_POST["email"];?><br>
   Comment: <?php echo $_POST["comment"];?>
   
